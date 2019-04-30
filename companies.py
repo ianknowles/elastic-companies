2133e7ec2a240167703350265c15fd4d9e147f92
ianknowles <ian@imknowles.co.uk>
2019-04-30 14:03:59 +0100

    Add basic readme and cleanup ingest, download data from companies house

--- companies.py
+++ companies.py
@@ -1,12 +1,17 @@
+import csv
+import shutil
+import urllib
+import zipfile
 from datetime import datetime
 from elasticsearch_dsl import Document, Date, Integer, Keyword, Text
 from elasticsearch_dsl.connections import connections
 
-# Define a default Elasticsearch client
+# Localhost for this example, point to your node on local network or online
 connections.create_connection(hosts=['localhost'])
 
 
 class Company(Document):
+	"""Class representing the searchable companies metadata"""
 	name = Text(analyzer='snowball', fields={'raw': Keyword()})
 	body = Text(analyzer='snowball')
 	tags = Keyword()
@@ -14,12 +19,14 @@ class Company(Document):
 	lines = Integer()
 
 	class Index:
+		"""The index that all instances of this metadata will be saved to"""
 		name = 'companies'
 		settings = {
 			"number_of_shards": 1,
 		}
 
 	def save(self, ** kwargs):
+		"""Saves the current item to the index"""
 		#self.lines = len(self.body.split())
 		return super(Company, self).save(** kwargs)
 
@@ -27,26 +34,36 @@ class Company(Document):
 		return datetime.now() >= self.published_from
 
 
-if __name__ == "__main__":
+def download():
+	"""Download the csv from companies house"""
+	companies_zip_url = 'http://download.companieshouse.gov.uk/BasicCompanyDataAsOneFile-2019-04-01.zip'
+	companies_zip_file = 'BasicCompanyDataAsOneFile-2019-04-01.zip'
+	with urllib.request.urlopen(companies_zip_url) as response, open(companies_zip_file, 'wb') as out_file:
+		shutil.copyfileobj(response, out_file)
+
+	with zipfile.ZipFile(companies_zip_file, 'r') as zipfilename:
+		zipfilename.extractall("data")
+
+
+def ingest():
+	"""Ingest everything in the csv into the cluster"""
 	# create the mappings in elasticsearch
 	Company.init()
 
-	import csv
-
 	row_count = 0
-	with open('company_list.tsv', 'r', encoding='utf8') as tsvin:
-		tsvin = csv.reader(tsvin, delimiter='\t')
+	with open('data\\BasicCompanyDataAsOneFile-2019-04-01.csv', 'r', encoding='utf8') as csvin:
+		companies = csv.reader(csvin)
 
-		for row in tsvin:
+		for company in companies:
 			row_count += 1
 
 	print('{time} Rows to ingest {x}'.format(x=row_count, time=datetime.now()))
 
 	row_count = 0
-	with open('company_list.tsv', 'r', encoding='utf8') as tsvin:
-		tsvin = csv.reader(tsvin, delimiter='\t')
+	with open('data\\BasicCompanyDataAsOneFile-2019-04-01.csv', 'r', encoding='utf8') as csvin:
+		companies = csv.reader(csvin)
 
-		for row in tsvin:
+		for row in companies:
 			try:
 				company = Company(name=row[0])
 				company.save()
@@ -68,3 +85,8 @@ if __name__ == "__main__":
 
 	# Display cluster health
 	print(connections.get_connection().cluster.health())
+
+
+if __name__ == "__main__":
+	download()
+	ingest()
